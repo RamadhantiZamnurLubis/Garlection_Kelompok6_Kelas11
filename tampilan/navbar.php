c5d764dd2130f1e6b7214c13f1ee28a49841ac41
Muhammad Razzaq Naufal <mrazzaq21@mhs.unsyiah.ac.id>
2023-05-04 21:45:52 +0700

    menambahkan halaman profile, mengubah tampilan home, menambahkan ikon pada home dan lainnya.

---
+++ tampilan/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="assets/garlection behbroh (1) 1.svg" alt="" width="49">
+      Garlection</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="profile.html"><i class="bi bi-person"></i> Profile</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
